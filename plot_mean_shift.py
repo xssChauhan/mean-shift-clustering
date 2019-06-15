cc25d6d6fa00d9d7cd3de6ce67df88341a0def7e
Shikhar Chauhan <shikhar.chauhan@live.com>
2019-06-15 18:46:29 +0530

    Creating base files from original implementation

---
+++ plot_mean_shift.py
@@ -0,0 +1,62 @@
+"""
+=============================================
+A demo of the mean-shift clustering algorithm
+=============================================
+
+MeanShift clustering aims to discover *blobs* in a smooth density of
+samples. It is a centroid based algorithm, which works by updating
+candidates for centroids to be the mean of the points within a given
+region. These candidates are then filtered in a post-processing stage
+to eliminate near-duplicates to form the final set of centroids.
+
+Reference:
+
+Dorin Comaniciu and Peter Meer, "Mean Shift: A robust approach toward
+feature space analysis". IEEE Transactions on Pattern Analysis and
+Machine Intelligence. 2002. pp. 603-619.
+
+"""
+print(__doc__)
+
+import numpy as np
+from sklearn.datasets.samples_generator import make_blobs
+
+from mean_shift_ import  *
+###############################################################################
+# Generate sample data
+centers = [[1, 1], [-1, -1], [1, -1]]
+X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
+
+###############################################################################
+# Compute clustering with MeanShift
+
+# The following bandwidth can be automatically detected using
+bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
+
+ms = MeanShift(bandwidth=bandwidth, bin_seeding=True, kernel="rbf")
+ms.fit(X)
+labels = ms.labels_
+cluster_centers = ms.cluster_centers_
+
+labels_unique = np.unique(labels)
+n_clusters_ = len(labels_unique)
+
+print("number of estimated clusters : %d" % n_clusters_)
+
+###############################################################################
+# Plot result
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+plt.figure(1)
+plt.clf()
+
+colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+for k, col in zip(range(n_clusters_), colors):
+    my_members = labels == k
+    cluster_center = cluster_centers[k]
+    plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=14)
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
